e2cd261fa49c2256d692eb1f30bfca8cb1b2a2a6
vek03 <vektromboni@gmail.com>
2025-11-23 13:37:55 -0300

    feat: add graylog

--- src/main/java/com/vekrest/veklambda/veklambda/function/ClientRegisteredFunction.java
+++ src/main/java/com/vekrest/veklambda/veklambda/function/ClientRegisteredFunction.java
@@ -1,16 +1,25 @@
 package com.vekrest.veklambda.veklambda.function;
 
 import com.vekrest.veklambda.veklambda.function.dto.ClientRegisteredMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import java.util.function.Consumer;
 
 @Component
 public class ClientRegisteredFunction {
+    private static final Logger LOG = LoggerFactory.getLogger(ClientRegisteredFunction.class);
+
     @Bean
     public Consumer<ClientRegisteredMessage> clientRegisteredConsumer() {
         return consumer -> {
-            System.out.println("Um cliente deseja realizar cadastro: " + consumer.name() + ", data de nascimento: " + consumer.birth() + ", cep: " + consumer.address().cep() + ", estado: " + consumer.address().state() + "!" );
+            LOG.info("Um cliente deseja realizar cadastro: nome: {}, data de nascimento: {}, cep: {}, estado: {}!",
+                    consumer.name(),
+                    consumer.birth(),
+                    consumer.address().cep(),
+                    consumer.address().state()
+            );
         };
     }
 }
