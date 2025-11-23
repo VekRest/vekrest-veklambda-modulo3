97cab2f6bb81fe9fa07816067df602a35d27e247
vek03 <vektromboni@gmail.com>
2025-11-23 12:13:05 -0300

    :tada: First Commit

---
+++ src/main/java/com/vekrest/veklambda/veklambda/VeklambdaApplication.java
@@ -0,0 +1,13 @@
+package com.vekrest.veklambda.veklambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeklambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeklambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vekrest/veklambda/veklambda/VeklambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vekrest.veklambda.veklambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeklambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
